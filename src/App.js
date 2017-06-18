cac31942c5dcfeacff2e6cb2b4e95495c5086365
Chris Graffagnino <graffwebdev@gmail.com>
2017-06-18 14:10:31 -0500

    Edit markup for MVP

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>:ZZ</h1>
+        <h1 className="AppTitle">How to Exit Vim</h1>
+        <div className="Container">
+          <div className="Card">
+            <h2>:wq</h2>
+            <p>write quit</p>
+          </div>
+          <div className="Card">
+            <h2>:q!</h2>
+            <p>quit without saving</p>
+          </div>
+        </div>
       </div>
     );
   }
